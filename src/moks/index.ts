38b81b744548feddab80be7f30cbe3b191588a97
drakerz <mr398@mail.ru>
2023-03-02 22:15:52 +0700

    Init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,317 @@
+export const users = [
+    {
+        "_id": "63fae66b154deca609d063f2",
+        "index": 0,
+        "guid": "d517bd9a-f429-4a97-8528-e97f20344f0a",
+        "isActive": false,
+        "balance": "$2,554.41",
+        "picture": "http://placehold.it/32x32",
+        "age": 30,
+        "eyeColor": "green",
+        "name": "Jenkins Crane",
+        "gender": "male",
+        "company": "ZORROMOP",
+        "email": "jenkinscrane@zorromop.com",
+        "phone": "+1 (862) 530-3553",
+        "address": "285 Prescott Place, Wakarusa, New York, 2380",
+        "about": "Cillum pariatur minim id sint est reprehenderit incididunt magna. Magna cupidatat fugiat non dolor ipsum qui sunt veniam qui ullamco esse. Enim dolor veniam ea ut velit voluptate eu eiusmod nisi qui aliqua.\r\n",
+        "registered": "2020-05-13T10:26:14 -07:00",
+        "latitude": 53.004408,
+        "longitude": 110.156252,
+        "tags": [
+            "esse",
+            "cillum",
+            "ad",
+            "aute",
+            "dolore",
+            "nostrud",
+            "sint"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Martin Grimes"
+            },
+            {
+                "id": 1,
+                "name": "Luna Meyers"
+            },
+            {
+                "id": 2,
+                "name": "Latoya Maldonado"
+            }
+        ],
+        "greeting": "Hello, Jenkins Crane! You have 4 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "63fae66b968edb1bdfa1af4e",
+        "index": 1,
+        "guid": "d577433f-180c-4685-bf61-f103f4aa8f7b",
+        "isActive": false,
+        "balance": "$3,499.79",
+        "picture": "http://placehold.it/32x32",
+        "age": 38,
+        "eyeColor": "brown",
+        "name": "Ruthie Jenkins",
+        "gender": "female",
+        "company": "RONBERT",
+        "email": "ruthiejenkins@ronbert.com",
+        "phone": "+1 (990) 583-2718",
+        "address": "959 Kane Street, Rosedale, Ohio, 6021",
+        "about": "Aliquip magna non consectetur aliqua et ut ex aliqua velit. Veniam nulla consequat ea laborum ipsum consequat. Sunt esse do sint irure ipsum. Elit est pariatur tempor aliqua adipisicing incididunt adipisicing minim culpa pariatur est aliqua officia deserunt. Laborum elit quis proident incididunt consequat commodo ipsum. Excepteur mollit ipsum culpa ut sint culpa. Id commodo ullamco duis eiusmod eiusmod id dolore exercitation sunt.\r\n",
+        "registered": "2021-06-08T01:34:46 -07:00",
+        "latitude": 41.620901,
+        "longitude": -48.69964,
+        "tags": [
+            "nulla",
+            "dolor",
+            "magna",
+            "in",
+            "eiusmod",
+            "nisi",
+            "id"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Merle Gilliam"
+            },
+            {
+                "id": 1,
+                "name": "Courtney Carver"
+            },
+            {
+                "id": 2,
+                "name": "Riddle Velasquez"
+            }
+        ],
+        "greeting": "Hello, Ruthie Jenkins! You have 3 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "63fae66b82913c7ad2641b96",
+        "index": 2,
+        "guid": "caf99c5e-1079-47c4-9862-587a6c4d18aa",
+        "isActive": false,
+        "balance": "$1,371.64",
+        "picture": "http://placehold.it/32x32",
+        "age": 20,
+        "eyeColor": "blue",
+        "name": "Young Robles",
+        "gender": "female",
+        "company": "TRIPSCH",
+        "email": "youngrobles@tripsch.com",
+        "phone": "+1 (887) 444-2459",
+        "address": "370 Pitkin Avenue, Soudan, North Carolina, 121",
+        "about": "Minim et elit irure aliquip enim incididunt proident aliquip sint nulla sint sint aute consequat. Consequat magna ea excepteur Lorem dolor nulla mollit ut. Pariatur minim ex mollit exercitation ut veniam consequat dolor. Dolore sint ad sunt nostrud et aute id laboris et voluptate velit eu. Irure do anim voluptate quis laborum reprehenderit occaecat veniam.\r\n",
+        "registered": "2015-05-07T07:02:31 -06:00",
+        "latitude": -12.649712,
+        "longitude": -30.214801,
+        "tags": [
+            "et",
+            "officia",
+            "laborum",
+            "in",
+            "minim",
+            "aliqua",
+            "cupidatat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Vilma Parks"
+            },
+            {
+                "id": 1,
+                "name": "Clara Bullock"
+            },
+            {
+                "id": 2,
+                "name": "Potts Holder"
+            }
+        ],
+        "greeting": "Hello, Young Robles! You have 4 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "63fae66b7a0c5ead21e3df88",
+        "index": 3,
+        "guid": "b5acf00e-59b8-445c-9dd1-21c7045ae14c",
+        "isActive": true,
+        "balance": "$1,445.15",
+        "picture": "http://placehold.it/32x32",
+        "age": 23,
+        "eyeColor": "brown",
+        "name": "Jo Brennan",
+        "gender": "female",
+        "company": "EXIAND",
+        "email": "jobrennan@exiand.com",
+        "phone": "+1 (894) 457-2652",
+        "address": "302 Sheffield Avenue, Alafaya, Virgin Islands, 3517",
+        "about": "Excepteur tempor minim eu tempor in amet minim aute officia. In sint enim sint sit fugiat fugiat aliquip eiusmod aliqua est. Pariatur ut dolor magna enim cupidatat commodo eu non tempor laborum sit anim sunt consequat. Labore irure reprehenderit eiusmod velit proident magna commodo labore quis. Ex irure velit non nisi ex id sit consectetur ex proident laborum duis enim veniam. Culpa ipsum ex occaecat eu ad consequat quis enim officia aliqua aliqua.\r\n",
+        "registered": "2022-01-10T06:28:09 -07:00",
+        "latitude": 53.274682,
+        "longitude": -53.757544,
+        "tags": [
+            "ex",
+            "tempor",
+            "mollit",
+            "non",
+            "labore",
+            "in",
+            "do"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Matilda Giles"
+            },
+            {
+                "id": 1,
+                "name": "Rae Morgan"
+            },
+            {
+                "id": 2,
+                "name": "Jimenez Zimmerman"
+            }
+        ],
+        "greeting": "Hello, Jo Brennan! You have 2 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "63fae66b5adb68e32038d6f0",
+        "index": 4,
+        "guid": "85ff6b61-42ea-4fdd-8cb0-ed49b4eeef3d",
+        "isActive": false,
+        "balance": "$1,205.23",
+        "picture": "http://placehold.it/32x32",
+        "age": 23,
+        "eyeColor": "green",
+        "name": "Douglas Monroe",
+        "gender": "male",
+        "company": "PODUNK",
+        "email": "douglasmonroe@podunk.com",
+        "phone": "+1 (979) 600-2220",
+        "address": "860 Clinton Street, Klondike, North Dakota, 3504",
+        "about": "Sint exercitation consectetur irure enim nostrud ullamco voluptate fugiat exercitation nisi mollit incididunt sunt. Commodo officia minim nisi elit. Enim excepteur et mollit officia do elit aliquip eiusmod labore occaecat ipsum magna. Nulla nulla nostrud ipsum officia ea. Minim sunt est irure magna. Ad non proident in minim qui aliquip in cupidatat anim aliquip amet.\r\n",
+        "registered": "2015-04-24T03:59:58 -06:00",
+        "latitude": -42.420007,
+        "longitude": 19.435798,
+        "tags": [
+            "irure",
+            "qui",
+            "commodo",
+            "velit",
+            "ex",
+            "incididunt",
+            "laborum"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Maura Trevino"
+            },
+            {
+                "id": 1,
+                "name": "Ramirez Lara"
+            },
+            {
+                "id": 2,
+                "name": "Morrow Finley"
+            }
+        ],
+        "greeting": "Hello, Douglas Monroe! You have 4 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "63fae66bce0de5557acd764a",
+        "index": 5,
+        "guid": "1768b74a-de41-4b21-94b4-0871b49c6a3b",
+        "isActive": true,
+        "balance": "$2,448.22",
+        "picture": "http://placehold.it/32x32",
+        "age": 29,
+        "eyeColor": "brown",
+        "name": "Curtis Dejesus",
+        "gender": "male",
+        "company": "EBIDCO",
+        "email": "curtisdejesus@ebidco.com",
+        "phone": "+1 (918) 498-3644",
+        "address": "192 Lois Avenue, Kohatk, Georgia, 6265",
+        "about": "Irure cupidatat aliquip dolor esse cillum laboris in do eu sunt. Duis sint quis nisi dolor consectetur eu velit culpa in. Pariatur qui enim dolore est dolor laboris fugiat ullamco est sint. Fugiat aute exercitation labore qui veniam laboris eiusmod irure non.\r\n",
+        "registered": "2019-07-08T01:28:33 -07:00",
+        "latitude": -58.114114,
+        "longitude": -128.003488,
+        "tags": [
+            "laboris",
+            "id",
+            "cupidatat",
+            "esse",
+            "anim",
+            "amet",
+            "qui"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Shepard Cooke"
+            },
+            {
+                "id": 1,
+                "name": "Maddox Walker"
+            },
+            {
+                "id": 2,
+                "name": "Watts Horn"
+            }
+        ],
+        "greeting": "Hello, Curtis Dejesus! You have 2 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "63fae66bba65aa91d49b1b93",
+        "index": 6,
+        "guid": "421d5a28-7d24-440a-b564-15bbfe820d50",
+        "isActive": false,
+        "balance": "$3,715.62",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "eyeColor": "green",
+        "name": "Gloria West",
+        "gender": "female",
+        "company": "ASSITIA",
+        "email": "gloriawest@assitia.com",
+        "phone": "+1 (989) 486-2187",
+        "address": "465 Kenmore Court, Leyner, California, 2141",
+        "about": "Tempor esse sunt dolore nulla enim incididunt non cupidatat enim. In qui irure non culpa occaecat et ea veniam proident nostrud consectetur. Occaecat cillum culpa culpa ea labore nisi id culpa fugiat voluptate. Est quis adipisicing eiusmod ullamco anim. Officia mollit duis ullamco ex tempor fugiat non duis proident in voluptate ex. Cupidatat officia fugiat laborum proident incididunt deserunt tempor dolore sint occaecat cupidatat Lorem.\r\n",
+        "registered": "2019-02-20T03:19:08 -07:00",
+        "latitude": -81.897046,
+        "longitude": 23.11837,
+        "tags": [
+            "consectetur",
+            "consequat",
+            "laboris",
+            "occaecat",
+            "non",
+            "pariatur",
+            "consectetur"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Alissa Gilbert"
+            },
+            {
+                "id": 1,
+                "name": "Mcknight Foreman"
+            },
+            {
+                "id": 2,
+                "name": "Lester Banks"
+            }
+        ],
+        "greeting": "Hello, Gloria West! You have 9 unread messages.",
+        "favoriteFruit": "banana"
+    }
+]
